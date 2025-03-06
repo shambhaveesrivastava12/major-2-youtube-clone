c933760e42c394c76083a12b8fd3df5bb82a65fd
shambhaveesrivastava12 <shambhaveesrivastava12@gmail.com>
2025-03-06 16:29:14 +0530

    fix: changes made

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import { Stack, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { logo } from "../utils/constants";
+import { SearchBar } from "./";
+
+const Navbar = () => (
+  <Stack
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{
+      position: "sticky",
+      background: "#000",
+      top: 0,
+      justifyContent: "space-between",
+    }}
+  >
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="logo" height={45} />
+    </Link>
+    <Typography
+      variant="h6"
+      color="white"
+      sx={{ flexGrow: 1, textAlign: "center" }}
+    >
+      Hello there, I am Shambhaveee
+    </Typography>
+    <SearchBar />
+  </Stack>
+);
+
+export default Navbar;
